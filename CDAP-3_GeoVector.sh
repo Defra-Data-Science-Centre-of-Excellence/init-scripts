051f523509ee0e9e94500b12e65ddbaa8523f772
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-07-26 14:12:57 +0100

    Update CDAP-3_GeoVector.sh

--- CDAP-3_GeoVector.sh
+++ CDAP-3_GeoVector.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src/
-wget -O - $URL"update.sh" | bash
-wget -O - $URL"base_libs.sh" | bash
-wget -O - $URL"geo_libs.sh" | bash
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
+wget -O - $URL/src/update.sh | bash
+wget -O - $URL/src/base_libs.sh" | bash
+wget -O - $URL/src/geo_libs.sh | bash
 
 echo """CDAP-3_GeoVector append to Spark config:
 spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
