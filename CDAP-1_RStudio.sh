29f1777a6a52232fc88aa83d96bcc5df77304aa2
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-07-26 14:11:52 +0100

    Update CDAP-1_RStudio.sh

--- CDAP-1_RStudio.sh
+++ CDAP-1_RStudio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-URL=https://raw.githubusercontent.com/https://github.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src/
-wget -O - $URL"update.sh" | bash
-wget -O - $URL"base_libs.sh" | bash
+URL=https://raw.githubusercontent.com/https://github.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
+wget -O - $URL/src/update.sh | bash
+wget -O - $URL/src/base_libs.sh | bash
