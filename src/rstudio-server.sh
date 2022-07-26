1fc3a058af3205ae7465e76867ad2758b192fb0a
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-07-26 13:22:18 +0100

    get $ARCH

--- src/rstudio-server.sh
+++ src/rstudio-server.sh
@@ -9,12 +9,12 @@ gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CB
 gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
 
 wget \
-  https://www.rstudio.org/download/latest/stable/server/bionic/rstudio-server-latest-amd64.deb \
-  -O rstudio-server-latest-amd64.deb
+  https://www.rstudio.org/download/latest/stable/server/bionic/rstudio-server-latest-$(dpkg --print-architecture).deb \
+  -O rstudio-server.deb
 
-sudo gdebi --non-interactive rstudio-server-latest-amd64.deb
+sudo gdebi --non-interactive rstudio-server.deb
 
-rm rstudio-server-latest-amd64.deb
+rm rstudio-server.deb
 
 sudo wget \
   https://raw.githubusercontent.com/jtsmith275/RStudioShiny-nginx/master/default \
