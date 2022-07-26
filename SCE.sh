57fec6209c4b61bafbe3496ce2ebd0d363656604
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-07-26 14:14:42 +0100

    Update SCE.sh

--- SCE.sh
+++ SCE.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src
-wget -O - "$URL/update.sh" | bash
-wget -O - "$URL/base_libs.sh" | bash
-wget -O - "$URL/nginx-server.sh" | bash
-wget -O - "$URL/rstudio-server.sh" | bash
-wget -O - "$URL/shiny-server.sh" | bash
-#wget -O - "$URL/jupyter-server.sh" | bash  # WIP
-(sudo crontab -l | sed '/^#/d'; echo "@monthly wget -qO- $URL/update.sh | bash") | sort -u | sudo crontab -
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
+wget -O - $URL/src/update.sh | bash
+wget -O - $URL/src/base_libs.sh | bash
+wget -O - $URL/src/nginx-server.sh | bash
+wget -O - $URL/src/rstudio-server.sh | bash
+wget -O - $URL/src/shiny-server.sh | bash
+#wget -O - $URL/src/jupyter-server.sh | bash  # WIP
+(sudo crontab -l | sed '/^#/d'; echo "@monthly wget -qO- $URL/src/update.sh | bash") | sort -u | sudo crontab -
 
 echo "nginx is" $(systemctl is-active nginx)
 echo "RStudio is" $(systemctl is-active rstudio-server)
