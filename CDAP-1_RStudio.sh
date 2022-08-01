f61482942da97979e2251469230e2a147b45f5ed
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-01 11:06:35 +0100

    ops typo

--- CDAP-1_RStudio.sh
+++ CDAP-1_RStudio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-URL=https://raw.githubusercontent.com/https://github.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
 wget -O - $URL/src/update.sh | bash
 wget -O - $URL/src/base_libs.sh | bash
