4166d3da5522f88591c210f5d4210a9bebb27126
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-01 14:09:32 +0100

    don't need `sort -u` just `uniq`

--- SCE.sh
+++ SCE.sh
@@ -6,7 +6,7 @@ wget -O - $URL/src/nginx-server.sh | bash
 wget -O - $URL/src/rstudio-server.sh | bash
 wget -O - $URL/src/shiny-server.sh | bash
 #wget -O - $URL/src/jupyter-server.sh | bash  # WIP
-(sudo crontab -l | sed '/^#/d'; echo "@monthly wget -qO- $URL/src/update.sh | bash") | sort -u | sudo crontab -
+(sudo crontab -l | sed '/^#/d'; echo "@monthly wget -qO- $URL/src/update.sh | bash") | uniq | sudo crontab -
 
 echo "nginx is" $(systemctl is-active nginx)
 echo "RStudio is" $(systemctl is-active rstudio-server)
