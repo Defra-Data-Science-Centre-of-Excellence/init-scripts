8271460b2695ce6f849cbc466c0edf05dbb13c82
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-01 11:04:39 +0100

    add RETICULATE_PYTHON to env for databricks

--- src/base_libs.sh
+++ src/base_libs.sh
@@ -18,6 +18,9 @@ apt-get install -y \
   r-cran-arrow r-cran-plotly \
   r-cran-packrat r-cran-rsconnect \
   r-cran-biocmanager r-cran-bs4dash r-cran-janitor r-cran-odbc r-cran-rgdal r-cran-rpostgres r-cran-srvyr r-cran-zoo
+if [[ "$(which python)" == *"databricks"* ]]; then
+  export RETICULATE_PYTHON=$(which python)
+fi
 
 # Python
 pip install \
