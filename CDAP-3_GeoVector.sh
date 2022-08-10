de2441af10a7d08cd321ceffef394b0a3ae56362
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-10 15:00:29 +0100

    gis is a bolt on not separate

--- CDAP-3_GeoVector.sh
+++ CDAP-3_GeoVector.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
-wget -O - $URL/src/update.sh | bash
-wget -O - $URL/src/base_libs.sh | bash
 wget -O - $URL/src/geo_libs.sh | bash
 
 echo """CDAP-3_GeoVector append to Spark config:
