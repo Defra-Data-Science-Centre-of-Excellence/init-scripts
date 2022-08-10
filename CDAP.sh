76aa4537c9dd07f1d5280c2aad8aba1fcfa0d180
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-10 19:20:44 +0100

    no cert specific

--- CDAP.sh
+++ CDAP.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-echo "check_certificate = off" >> ~/.wgetrc
-
 URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
 
 FILE=1_rstudio_test.sh
 FOLDER=/dbfs/databricks/scripts/$FILE  # required for appending to /dbfs/
-wget $URL/src/update.sh -qO-> $FILE
-wget $URL/src/base_libs.sh -qO->> $FILE
+wget -O- --no-check-certificate $URL/src/update.sh > $FILE
+wget -O- --no-check-certificate $URL/src/base_libs.sh >> $FILE
 mv $FILE $FOLDER$FILE
 
 FILE=/dbfs/databricks/scripts/3_geovector_test.sh
-wget $URL/src/gis_libs.sh -qO-> $FILE
+wget -O- --no-check-certificate $URL/src/gis_libs.sh > $FILE
