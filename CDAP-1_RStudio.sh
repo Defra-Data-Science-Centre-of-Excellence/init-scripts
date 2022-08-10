5d21528906cf3f3ed730d9c11d6f7549c07a3382
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-10 15:04:00 +0100

    auto update monthly (#1)
    adding cronjob

--- CDAP-1_RStudio.sh
+++ CDAP-1_RStudio.sh
@@ -2,3 +2,4 @@
 URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
 wget -O - $URL/src/update.sh | bash
 wget -O - $URL/src/base_libs.sh | bash
+(sudo crontab -l | sed '/^#/d'; echo "@monthly wget -qO- $URL/src/update.sh | bash") | uniq | sudo crontab -
