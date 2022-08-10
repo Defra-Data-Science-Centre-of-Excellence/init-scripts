ef79de32e6adca812f104e328937695b795f3baf
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-10 16:54:17 +0100

    no cert check default

--- CDAP-1_RStudio.sh
+++ CDAP-1_RStudio.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "check_certificate = off" >> ~/.wgetrc
+
 URL=http://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
 wget -O- $URL/src/update.sh | bash
 wget -O- $URL/src/base_libs.sh | bash
