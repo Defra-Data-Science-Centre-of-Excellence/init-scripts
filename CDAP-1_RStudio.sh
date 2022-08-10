9ee2051a563b4c2e146d41982f863f5eb2904cad
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-10 16:51:27 +0100

    no certificate check

--- CDAP-1_RStudio.sh
+++ CDAP-1_RStudio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
-wget -O - $URL/src/update.sh | bash
-wget -O - $URL/src/base_libs.sh | bash
+URL=http://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
+wget -O- $URL/src/update.sh | bash
+wget -O- $URL/src/base_libs.sh | bash
 (sudo crontab -l | sed '/^#/d'; echo "@monthly wget -qO- $URL/src/update.sh | bash") | uniq | sudo crontab -
