ab6f67ea734a7f7752cd9c52b522bef86be63556
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-12 10:37:11 +0100

    Create create_init_scripts.sh

---
+++ rsc/create_init_scripts.sh
@@ -0,0 +1,106 @@
+%sh
+FOLDER=/dbfs/databricks/scripts
+FILE0=$FOLDER/update.sh
+FILE1=$FOLDER/1_rstudio_test.sh
+FILE3=$FOLDER/3_geovector_test.sh
+
+
+
+echo """#!/bin/bash
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt-get update -y
+sudo apt-get full-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autopurge -y
+""" > $FILE0
+
+
+
+echo """#!/bin/bash
+
+# Repos
+sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+sudo add-apt-repository ppa:ubuntugis/ppa
+
+# Update
+sudo apt-get update
+pip install -U pip
+
+# C
+sudo apt-get install -y \
+  libsqlite3-mod-spatialite
+
+# R
+apt-get install -y \
+  r-cran-renv r-cran-devtools r-cran-rstudioapi r-cran-reticulate r-cran-packrat r-cran-rsconnect \
+  r-cran-dt r-cran-shinyjs r-cran-shinydashboard r-cran-shinycssloaders \
+  r-cran-sf r-cran-raster r-cran-leaflet \
+  r-cran-arrow r-cran-plotly \
+  r-cran-biocmanager r-cran-janitor r-cran-odbc r-cran-rgdal r-cran-rpostgres r-cran-srvyr r-cran-zoo
+
+# Python
+pip install \
+  pandas matplotlib openpyxl \
+  spatialite rtree pyproj geopandas geocube
+  
+# Reticulate
+echo RETICULATE_PYTHON=$(which python3) >> /etc/environment
+""" > $FILE1
+
+
+
+echo """#!/bin/bash
+# Sedona
+## requires spark config:
+### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
+### spark.serializer org.apache.spark.serializer.KryoSerializer
+### spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
+DIR="/dbfs/databricks/jars"
+if [ ! -d $DIR ]; then
+  mkdir -p $DIR
+  curl -o $DIR/geotools-wrapper-1.1.0-25.2.jar "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.1.0-25.2/geotools-wrapper-1.1.0-25.2.jar"
+  curl -o $DIR/sedona-python-adapter-3.0_2.12-1.1.1-incubating.jar "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/1.1.1-incubating/sedona-python-adapter-3.0_2.12-1.1.1-incubating.jar"
+  curl -o $DIR/sedona-viz-3.0_2.12-1.1.1-incubating.jar "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.12/1.1.1-incubating/sedona-viz-3.0_2.12-1.1.1-incubating.jar"
+fi
+cp $DIR/*.jar /databricks/jars
+pip install \
+  apache-sedona
+
+# Mosaic
+pip install \
+  databricks-mosaic
+
+# PySpark Vector Files
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install -y \
+  libudunits2-dev \
+  libgdal-dev \
+  libgeos-dev \
+  libproj-dev \
+  libspatialindex-dev
+pip install \
+  pyorgio \
+  pyspark-vector-files
+''' > $FILE3
+
+
+
+echo """TODO:
+
+- Logging:
+dbfs:/databricks/logs/0_support
+dbfs:/databricks/logs/1_rstudio
+dbfs:/databricks/logs/2_notebook
+dbfs:/databricks/logs/3_geovector
+dbfs:/databricks/logs/4_training
+
+- Init Scripts:
+dbfs:/databricks/scripts/1_rstudio.sh
+dbfs:/databricks/scripts/3_geovector.sh
+
+- 3_GeoVector Append to Spark config:
+spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
+spark.serializer org.apache.spark.serializer.KryoSerializer
+spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
+"""
