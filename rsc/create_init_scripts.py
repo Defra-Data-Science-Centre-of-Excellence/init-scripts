8e4645ae3349042c4d43c0273becbcaa70d78ffc
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-22 10:48:55 +0100

    replace curl with wget.  wget>curl

--- rsc/create_init_scripts.py
+++ rsc/create_init_scripts.py
@@ -52,13 +52,11 @@ script = r'''#!/bin/bash
 ### spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
 ### spark.serializer org.apache.spark.serializer.KryoSerializer
 ### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
-DIR="/dbfs/databricks/jars"
-if [ ! -d $DIR ]; then
-  mkdir -p $DIR
-  curl -o $DIR/geotools-wrapper-1.1.0-25.2.jar "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.1.0-25.2/geotools-wrapper-1.1.0-25.2.jar"
-  curl -o $DIR/sedona-python-adapter-3.0_2.12-1.1.1-incubating.jar "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/1.1.1-incubating/sedona-python-adapter-3.0_2.12-1.1.1-incubating.jar"
-  curl -o $DIR/sedona-viz-3.0_2.12-1.1.1-incubating.jar "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.12/1.1.1-incubating/sedona-viz-3.0_2.12-1.1.1-incubating.jar"
-fi
+DIR=/dbfs/databricks/jars
+mkdir -p $DIR
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.1.0-25.2/geotools-wrapper-1.1.0-25.2.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/1.1.1-incubating/sedona-python-adapter-3.0_2.12-1.1.1-incubating.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.12/1.1.1-incubating/sedona-viz-3.0_2.12-1.1.1-incubating.jar"
 cp $DIR/*.jar /databricks/jars
 pip install \
   apache-sedona
