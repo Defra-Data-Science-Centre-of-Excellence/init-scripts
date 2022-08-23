d6ceb1e383c4fd6ec583447dc420565051f06718
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-23 10:15:51 +0100

    Delete CDAP-1_RStudio.sh

--- CDAP-1_RStudio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "check_certificate = off" >> ~/.wgetrc
-
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
-wget -O- $URL/src/update.sh | bash
-wget -O- $URL/src/base_libs.sh | bash
-(sudo crontab -l | sed '/^#/d'; echo "@monthly wget -qO- $URL/src/update.sh | bash") | uniq | sudo crontab -
