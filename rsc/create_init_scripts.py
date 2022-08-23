47ee85cee5817b16378ecccca63a46ff2a6b07dc
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-23 12:37:38 +0100

    add spark config via init

--- rsc/create_init_scripts.py
+++ rsc/create_init_scripts.py
@@ -48,10 +48,6 @@ dbutils.fs.put(file_func(filename), script, overwrite=True)
 filename = '3_geovector.sh'
 script = r'''#!/bin/bash
 # Sedona
-## requires spark config:
-### spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
-### spark.serializer org.apache.spark.serializer.KryoSerializer
-### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
 DIR=/dbfs/databricks/jars
 mkdir -p $DIR
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.1.0-25.2/geotools-wrapper-1.1.0-25.2.jar"
@@ -60,6 +56,12 @@ wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/
 cp $DIR/*.jar /databricks/jars
 pip install \
   apache-sedona
+echo '''[driver] {
+  "spark.kryo.registrator" = "org.apache.sedona.core.serde.SedonaKryoRegistrator"
+  "spark.serializer" = "org.apache.spark.serializer.KryoSerializer"
+  "spark.sql.extensions" = "org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions"
+  "spark.sql.execution.arrow.pyspark.enabled" = "true"
+}''' > /databricks/driver/conf/00-custom-spark-driver-defaults.conf
 
 # Mosaic
 pip install \
