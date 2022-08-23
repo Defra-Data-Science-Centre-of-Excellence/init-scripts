d03440296e43b96dc8f57fd0d18f998ceff817a8
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-23 11:24:39 +0100

    echo over wget

--- src/rstudio-server.sh
+++ src/rstudio-server.sh
@@ -1,5 +1,50 @@
+NGINX_DEFAULT='''
+map $http_upgrade $connection_upgrade {   
+   default upgrade;
+   ''	close;
+ }
+server {
+	listen 80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	location / {
+		try_files $uri $uri/ =404;
+	}
+  location /rstudio/ {
+    rewrite ^/rstudio/(.*)$ /$1 break;
+    proxy_pass http://127.0.0.1:8787;
+    proxy_redirect http://127.0.0.1:8787/ $scheme://$host/rstudio/;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection $connection_upgrade;
+    client_max_body_size 0;
+    proxy_read_timeout 20d;
+  }
+  location /shiny/ {
+    rewrite ^/shiny/(.*)$ /$1 break;
+    proxy_pass http://127.0.0.1:3838;
+    proxy_redirect http://127.0.0.1:3838/ $scheme://$http_host/shiny;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection $connection_upgrade;
+    proxy_read_timeout 20d;
+    proxy_buffering off;
+  }
+	location /jupyter/ {
+    proxy_pass http://127.0.0.1:8000;
+    proxy_redirect   off;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header Host $host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    # websocket headers
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection $connection_upgrade;
+	}
+}
+'''
 
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
 
 sudo apt install -y \
   r-base r-base-dev \
@@ -15,14 +60,9 @@ ARCH=$(dpkg --print-architecture)
 wget \
   https://www.rstudio.org/download/latest/stable/server/bionic/rstudio-server-latest-$ARCH.deb \
   -O rstudio-server.deb
-
 sudo gdebi --non-interactive rstudio-server.deb
-
 rm rstudio-server.deb
-
-sudo wget \
-  $URL/rsc/nginx_default \
-  -O /etc/nginx/sites-enabled/default
+echo $NGINX_DEFAULT > /etc/nginx/sites-enabled/default
 
 
 sudo systemctl daemon-reload
