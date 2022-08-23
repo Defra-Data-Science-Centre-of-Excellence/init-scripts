62101bbf02f5dcda9e19f39649660daa5a2df357
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-23 10:58:35 +0100

    drop quarto

--- src/base_libs.sh
+++ src/base_libs.sh
@@ -12,24 +12,13 @@ sudo apt-get install -y \
 
 # R
 apt-get install -y \
-  r-cran-renv r-cran-devtools r-cran-rstudioapi r-cran-reticulate \
+  r-cran-renv r-cran-devtools r-cran-rstudioapi r-cran-reticulate r-cran-packrat r-cran-rsconnect \
   r-cran-dt r-cran-shinyjs r-cran-shinydashboard r-cran-shinycssloaders \
   r-cran-sf r-cran-raster r-cran-leaflet \
   r-cran-arrow r-cran-plotly \
-  r-cran-packrat r-cran-rsconnect \
   r-cran-biocmanager r-cran-janitor r-cran-odbc r-cran-rgdal r-cran-rpostgres r-cran-srvyr r-cran-zoo
 
 # Python
 pip install \
   pandas matplotlib openpyxl \
   spatialite rtree pyproj geopandas geocube
-
-# Quarto
-## https://docs.rstudio.com/resources/install-quarto/
-FILE=quarto-linux-amd64.deb
-wget -O $FILE https://quarto.org/download/latest/$FILE
-sudo apt-get install -y \
-  gdebi-core
-sudo gdebi -n $FILE
-rm $FILE
-/usr/local/bin/quarto check
