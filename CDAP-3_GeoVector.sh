2bbff76143737e4b9b5149784594a3e7c36d3968
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-23 10:15:44 +0100

    Delete CDAP-3_GeoVector.sh

--- CDAP-3_GeoVector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
-wget -O - $URL/src/geo_libs.sh | bash
-
-echo """CDAP-3_GeoVector append to Spark config:
-spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
-spark.serializer org.apache.spark.serializer.KryoSerializer
-spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
-"""
