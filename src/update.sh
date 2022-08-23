d6af98857481895ea0e47e79da5bbd1171a73fdd
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-23 12:26:31 +0100

    Update update.sh

--- src/update.sh
+++ src/update.sh
@@ -3,5 +3,7 @@ sudo apt-get update -y
 sudo apt-get full-upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autopurge -y
-pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U 
+sudo apt-get autoclean -y
 
+python -m pip install -U pip
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 python -m pip install -U 
