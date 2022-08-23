4021e62462091446bd77ae1ff28da7e95e10d42c
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-23 16:37:07 +0100

    Bumped Sedona version from 1.1.1 to 1.2.0

--- rsc/create_init_scripts.py
+++ rsc/create_init_scripts.py
@@ -55,8 +55,8 @@ script = r'''#!/bin/bash
 DIR=/dbfs/databricks/jars
 mkdir -p $DIR
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.1.0-25.2/geotools-wrapper-1.1.0-25.2.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/1.1.1-incubating/sedona-python-adapter-3.0_2.12-1.1.1-incubating.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.12/1.1.1-incubating/sedona-viz-3.0_2.12-1.1.1-incubating.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/1.2.0-incubating/sedona-python-adapter-3.0_2.12-1.2.0-incubating.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.12/1.2.0-incubating/sedona-viz-3.0_2.12-1.2.0-incubating.jar"
 cp $DIR/*.jar /databricks/jars
 pip install \
   apache-sedona
