fb67dec4c0267d48038abd58c649dea3cc6a6176
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-09-07 13:40:17 +0100

    Update create_init_scripts.py

--- rsc/create_init_scripts.py
+++ rsc/create_init_scripts.py
@@ -54,9 +54,10 @@ script = r'''#!/bin/bash
 ### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
 DIR=/dbfs/databricks/jars
 mkdir -p $DIR
+VERSION="1.2.1"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.1.0-25.2/geotools-wrapper-1.1.0-25.2.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/1.2.0-incubating/sedona-python-adapter-3.0_2.12-1.2.0-incubating.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.12/1.2.0-incubating/sedona-viz-3.0_2.12-1.2.0-incubating.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/$VERSION-incubating/sedona-python-adapter-3.0_2.12-$VERSION-incubating.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.12/$VERSION-incubating/sedona-viz-3.0_2.12-$VERSION-incubating.jar"
 cp $DIR/*.jar /databricks/jars
 pip install \
   apache-sedona
