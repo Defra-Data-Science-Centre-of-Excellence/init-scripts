6f78e36d9bcba9fd3613baa8ac58aff05df27540
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-03-29 17:32:04 +0100

    added bin_libs mode

--- rsc/app.r
+++ rsc/app.r
@@ -1,18 +1,35 @@
 library('shiny')
 
+
 if (require('DT')) {
   tableOutput = DT::dataTableOutput
   renderTable = DT::renderDataTable
 }
 
-data = as.data.frame(installed.packages())
 
-ui = {
-  tableOutput('table')
+data = {
+  cdata = system('apt list --installed', T,F,T)
+  cdata = cdata[2:length(cdata)]
+  cdata = as.data.frame(cdata)
+
+  rdata = as.data.frame(installed.packages())
+  
+  list(
+    'r_libs' = rdata,
+    'bin_libs' = cdata
+  )
 }
+
+
+ui = fluidPage(
+  selectInput('lib', NULL, choices=names(data)),
+  tableOutput('table')
+)
+
 
 server = function(input, output) {
-  output$table <- renderTable(data)
+  output$table <- renderTable(data[[input$lib]])
 }
+
 
 shinyApp(ui, server)
