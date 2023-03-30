105941ea92ee2efdc065c5cb0a276ac24f504e49
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-03-30 10:46:48 +0100

    improved dpkg cols

--- rsc/app.r
+++ rsc/app.r
@@ -1,10 +1,7 @@
-library('shiny')
-
-
-if (require('DT')) {
-  tableOutput = DT::dataTableOutput
-  renderTable = DT::renderDataTable
-}
+#install.packages(c('shiny', 'stringr', 'DT'))
+library(shiny)
+library(stringr)
+library(DT)
 
 
 data = {list(
@@ -12,25 +9,27 @@ data = {list(
       as.data.frame(installed.packages())
     },
     'apt_libs' = {
-      apt = system('apt list --installed', T,F,T)
+      apt = system('apt list', T,F,T)
       apt = apt[2:length(apt)]
       as.data.frame(apt)
     },
     'dpkg_libs' = {
-      read.csv(text=system("echo 'Package,Version,Section,Homepage,Source'; dpkg-query -Wf '${Package},${Version},${Section},${Homepage},${Source}\\n' | sort -n", T,F,T))
+      # Architecture,Bugs,Conffiles,Config-Version,Conflicts,Breaks,Depends,Description,Enhances,Protected,Essential,Filename,Homepage,Installed-Size,MD5sum,MSDOS-Filename,Maintainer,Origin,Package,Pre-Depends,Priority,Provides,Recommends,Replaces,Revision,Section,Size,Source,Status,Suggests,Tag,Triggers-Awaited,Triggers-Pending,Version
+      cols = 'Package\tVersion\tStatus\tArchitecture\tSection\tInstalled-Size\tSource\tMaintainer\tHomepage'
+      cmd = paste0("echo '",cols,"';dpkg-query -Wf '\"${",str_replace_all(cols,'\t','}"\t\"${'),"}\"\n'|sort -n")
+      read.table(text=system(cmd, T,F,T), header=TRUE)
     }
 )}
 
 
-
-ui = fluidPage(
+ui = {fluidPage(
   selectInput('lib', NULL, choices=names(data)),
-  tableOutput('table')
-)
+  dataTableOutput('table')
+)}
 
 
 server = function(input, output) {
-  output$table <- renderTable(data[[input$lib]])
+  output$table <- renderDataTable(data[[input$lib]])
 }
 
 
