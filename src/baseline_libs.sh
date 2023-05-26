7c9720e6a002794eada649b707fd50a56959518f
Bring salmon <128599540+pantelisindefra@users.noreply.github.com>
2023-05-26 10:52:54 +0100

    Update baseline_libs.sh

--- src/baseline_libs.sh
+++ src/baseline_libs.sh
@@ -35,6 +35,7 @@ apt-get install -y \
   r-cran-dt r-cran-shinyjs r-cran-shinydashboard r-cran-shinycssloaders \
   r-cran-sf r-cran-raster r-cran-leaflet \
   r-cran-arrow r-cran-plotly \
+  r-cran-styler \
   r-cran-biocmanager r-cran-janitor r-cran-odbc r-cran-rgdal r-cran-rpostgres r-cran-srvyr r-cran-zoo
 
 # Python
