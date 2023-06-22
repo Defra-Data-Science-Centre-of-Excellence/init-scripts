0dcdc5dad653f5a96ff18b9620043d7583d20b7f
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-06-22 09:56:18 +0100

    sedona 1.4.0 (non incubating)

--- src/gis_libs.sh
+++ src/gis_libs.sh
@@ -5,10 +5,10 @@
 ### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
 DIR=/dbfs/databricks/jars
 mkdir -p $DIR
-VERSION="1.3.1"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.3.0-27.2/geotools-wrapper-1.3.0-27.2.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.13/$VERSION-incubating/sedona-python-adapter-3.0_2.13-$VERSION-incubating.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.13/$VERSION-incubating/sedona-viz-3.0_2.13-$VERSION-incubating.jar"
+VERSION="1.4.0"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.4.0-28.2/geotools-wrapper-1.4.0-28.2.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.13/$VERSION/sedona-python-adapter-3.0_2.13-$VERSION.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.13/$VERSION/sedona-viz-3.0_2.13-$VERSION.jar"
 cp $DIR/*.jar /databricks/jars
 pip install \
   apache-sedona==$VERSION
