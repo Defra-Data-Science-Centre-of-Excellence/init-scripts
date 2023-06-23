f0c4b4b1f062ec64805178cd15e85046ce60fa12
aw-west-defra <aw-west-defra>
2023-06-23 13:20:13 +0000

    fixed clear extra folders

--- src/dbfs_clean.py
+++ src/dbfs_clean.py
@@ -1,9 +1,15 @@
 # Databricks notebook source
 # MAGIC %sh
 # MAGIC # Clear extra folders
-# MAGIC ls /dbfs/ | awk \
-# MAGIC   '!/databricks/ && !/databricks-datasets/ && !/databricks-results/ && !/FileStore/ && !/mnt/ && !/tmp/ && !/user/ ' \
-# MAGIC   | xargs rm
+# MAGIC find /dbfs/ -mindepth 1 -maxdepth 1 -type d \
+# MAGIC   ! -name databricks \
+# MAGIC   ! -name databricks-datasets \
+# MAGIC   ! -name databricks-results \
+# MAGIC   ! -name FileStore \
+# MAGIC   ! -name mnt \
+# MAGIC   ! -name tmp \
+# MAGIC   ! -name user \
+# MAGIC   | rm -r
 # MAGIC
 # MAGIC # Clear 30 days old FileStore
 # MAGIC find /dbfs/FileStore/* -atime 30 | xargs rm 
