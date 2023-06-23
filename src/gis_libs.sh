948fc6bac49fae660db87df54c25dd962b69e20b
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-06-23 20:03:23 +0100

    typo

--- src/gis_libs.sh
+++ src/gis_libs.sh
@@ -8,8 +8,8 @@ SPARK="3.0"
 SCALA="2.12"
 SEDONA="1.4.0"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.4.0-28.2/geotools-wrapper-1.4.0-28.2.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-$SPARK_$SCALA/$SEDONA/sedona-spark-shaded-$SPARK_$SCALA-$SEDONA.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-$SPARK_$SCALA/$SEDONA/sedona-viz-$SPARK_$SCALA-$SEDONA.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
 pip install \
   apache-sedona==$SEDONA
 
