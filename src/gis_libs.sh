125f87780619b04afaa2cc5002cecea96afb1670
aw-west-defra <aw-west-defra>
2023-06-23 10:59:18 +0000

    sedona 1.4.0 and direct dl instead of tmp

--- src/gis_libs.sh
+++ src/gis_libs.sh
@@ -3,13 +3,12 @@
 ### spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
 ### spark.serializer org.apache.spark.serializer.KryoSerializer
 ### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
-DIR=/dbfs/databricks/jars
+DIR=/databricks/jars
 mkdir -p $DIR
 VERSION="1.4.0"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.4.0-28.2/geotools-wrapper-1.4.0-28.2.jar"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.13/$VERSION/sedona-python-adapter-3.0_2.13-$VERSION.jar"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.0_2.13/$VERSION/sedona-viz-3.0_2.13-$VERSION.jar"
-cp $DIR/*.jar /databricks/jars
 pip install \
   apache-sedona==$VERSION
 
