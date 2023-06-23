9f2955fa6dd3917f646adb9b54cced51e07eb65a
aw-west-defra <aw-west-defra>
2023-06-23 11:24:19 +0000

    hidden rootcert

--- CDAP.sh
+++ CDAP.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main
 FOLDER=/dbfs/databricks/scripts
+
+#wget --no-check-certificate -NP $DIR $URL/src/rootcert.sh  # hidden
 wget --no-check-certificate -NP $DIR $URL/src/update.sh
 wget --no-check-certificate -NP $DIR $URL/src/base_libs.sh
 wget --no-check-certificate -NP $DIR $URL/src/gis_libs.sh
