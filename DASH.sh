9b2f638ed35309e564cc84f7cf07df01120320f7
aw-west-defra <aw-west-defra>
2023-06-23 18:10:43 +0000

    read direct from github

--- DASH.sh
+++ DASH.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -ex
+echo $DB_CLUSTER_NAME
 
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/aw-dev
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/aw-dev/src
 
 wget $URL/rootcert.sh -O- | bash
-# . $DIR/update.sh
-# . $DIR/base_libs.sh
-# if [[ "$DB_CLUSTER_ID" == *"Geo"* ]]; then
-#   . $DIR/gis_libs.sh
-# fi
+wget $URL/update.sh -O- | bash
+wget $URL/base_libs.sh -O- | bash
+if [[ "$DB_CLUSTER_NAME" == *"Geo"* ]]; then
+  wget $URL/gis_libs.sh -O- | bash
+fi
