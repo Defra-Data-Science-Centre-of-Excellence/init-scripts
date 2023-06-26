ca333ff1396ea9b17e6224867e3e96e8baf4d71f
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-06-26 12:09:41 +0100

    added rootcert

--- DASH.sh
+++ DASH.sh
@@ -4,7 +4,10 @@ echo $DB_CLUSTER_NAME
 
 URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/aw-dev/src
 
-wget $URL/rootcert.sh -O- | bash
+rootcert=/dbfs/databricks/init-scripts/rootcert.sh
+if  [[ -f $rootcert ]]; then
+  . $rootcert
+fi
 wget $URL/update.sh -O- | bash
 wget $URL/base_libs.sh -O- | bash
 if [[ "$DB_CLUSTER_NAME" == *"Geo"* ]]; then
