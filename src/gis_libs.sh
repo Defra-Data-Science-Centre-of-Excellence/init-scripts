56fa2d6a05da040c4980f032d05ce615ab6c08ef
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-06-27 11:29:31 +0100

    sedona-1.4.1

--- src/gis_libs.sh
+++ src/gis_libs.sh
@@ -6,7 +6,7 @@
 DIR=/databricks/jars
 SPARK="3.0"
 SCALA="2.12"
-SEDONA="1.4.0"
+SEDONA="1.4.1"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.4.0-28.2/geotools-wrapper-1.4.0-28.2.jar"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
