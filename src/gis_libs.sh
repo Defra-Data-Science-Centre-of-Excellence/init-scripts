981726735eb9594d5d86c5743783560c6147954d
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-06-27 18:21:19 +0100

    sedona-1.4.1 (#23)
    * sedona-1.4.1
    
    * spark version for sedona
    
    * spark version note
    
    * using pyspark version

--- src/gis_libs.sh
+++ src/gis_libs.sh
@@ -4,9 +4,14 @@
 ### spark.serializer org.apache.spark.serializer.KryoSerializer
 ### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
 DIR=/databricks/jars
-SPARK="3.0"
+export PYSPARK_VERSION=$(python -c "import pyspark; print(pyspark.__version__[:3])")
+if [[ $PYSPARK_VERSION == "3.4" ]]; then
+  SPARK="3.4"
+else
+  SPARK="3.0"
+fi
 SCALA="2.12"
-SEDONA="1.4.0"
+SEDONA="1.4.1"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.4.0-28.2/geotools-wrapper-1.4.0-28.2.jar"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
