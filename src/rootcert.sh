d250550febd7800d92e4d328c3fa90e91a1ab095
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-08-21 14:53:10 +0100

    original rootcert without certs

--- src/rootcert.sh
+++ src/rootcert.sh
@@ -1,1 +1,42 @@
-echo "This is the rootcert.sh placeholder."
+#!/bin/bash
+
+
+cat << 'EOF' > /usr/local/share/ca-certificates/cscrootcert.crt
+-----BEGIN CERTIFICATE-----
+...
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+...
+-----END CERTIFICATE-----
+EOF
+
+
+update-ca-certificates
+
+
+PEM_FILE="/etc/ssl/certs/cscrootcert.pem"
+PASSWORD=""
+JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+KEYSTORE="$JAVA_HOME/lib/security/cacerts"
+
+
+CERTS=$(grep 'END CERTIFICATE' $PEM_FILE| wc -l)
+
+
+# To process multiple certs with keytool, you need to extract
+# each one from the PEM file and import it into the Java KeyStore.
+
+
+for N in $(seq 0 $(($CERTS - 1))); do
+  ALIAS="$(basename $PEM_FILE)-$N"
+  echo "Adding to keystore with alias:$ALIAS"
+  cat $PEM_FILE |
+    awk "n==$N { print }; /END CERTIFICATE/ { n++ }" |
+    keytool -noprompt -import -trustcacerts             -alias $ALIAS -keystore $KEYSTORE -storepass $PASSWORD
+done
+
+
+echo "export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt" >> /databricks/spark/conf/spark-env.sh
+
+
+/databricks/spark/scripts/restart_dbfs_fuse_daemon.sh
