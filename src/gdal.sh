c440a2a383270207078f399cc273eeef554439e6
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-08-24 13:58:18 +0100

    Create gdal.sh

---
+++ src/gdal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DIR=/databricks/miniconda
+mkdir $DIR
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $DIR/miniconda_installer.sh
+bash $DIR/miniconda_installer.sh -b -u -f -p /dbfs/databricks/miniconda
+
+export PATH=$PATH:$DIR/bin
+
+$DIR/bin/conda update --all
+$DIR/bin/conda install -c conda-forge libgdal-arrow-parquet gdal
