1a37316f9c0f2a60db19190737b0d8b233a32783
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-10-25 11:06:16 +0100

    ogr settings

--- src/gdal.sh
+++ src/gdal.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-DIR=/databricks/miniconda
-mkdir $DIR
+export MINICONDA_DIR=/databricks/miniconda
+mkdir -p $MINICONDA_DIR
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $DIR/miniconda_installer.sh
-bash $DIR/miniconda_installer.sh -b -u -f -p $DIR
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $MINICONDA_DIR/miniconda_installer.sh
+bash $MINICONDA_DIR/miniconda_installer.sh -b -u -f -p $MINICONDA_DIR
 
-export PATH=$PATH:$DIR/bin
+$MINICONDA_DIR/bin/conda update --all
+$MINICONDA_DIR/bin/conda install -c conda-forge libgdal-arrow-parquet gdal
 
-$DIR/bin/conda update --all
-$DIR/bin/conda install -c conda-forge libgdal-arrow-parquet gdal
+# ogr settings
+export TMPDIR=/tmp
+export PROJ_LIB=/databricks/miniconda/share/proj
+export OGR_GEOMETRY_ACCEPT_UNCLOSED_RING=NO
