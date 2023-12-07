7cd1a35e0e74893be61ea0e3039f670f7fe0e154
James Duffy <106590727+jpd-defra@users.noreply.github.com>
2023-12-07 17:11:21 +0000

    Update DASH.sh

--- DASH.sh
+++ DASH.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 echo $DB_CLUSTER_NAME
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/esd-init-scripts/main/src
 
 wget $URL/rootcerts.sh -O- | bash
 wget $URL/update.sh -O- | bash 
