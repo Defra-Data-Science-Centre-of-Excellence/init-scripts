01933637e5ff5ea7d09d0d6035ac19404f5b49fb
James Duffy <106590727+jpd-defra@users.noreply.github.com>
2024-01-24 14:40:09 +0000

    new style inits

---
+++ ESD_15min_greenspace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+echo $DB_CLUSTER_NAME
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/esd-init-scripts/main/src
+
+wget $URL/rootcerts.sh -O- | bash
+wget $URL/c_dependencies.sh -O- | bash
+wget $URL/sedona_150.sh -O- | bash
+
+# other libraries
+pip install \
+  pyogrio==0.7.2
