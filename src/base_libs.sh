fbeae350cbba1019b3f58032c316306d4afe2257
Bring salmon <128599540+pantelisindefra@users.noreply.github.com>
2024-02-07 12:29:19 +0000

    Update base_libs.sh

--- src/base_libs.sh
+++ src/base_libs.sh
@@ -29,7 +29,10 @@ apt-get install -y \
   r-cran-dt r-cran-shinyjs r-cran-shinydashboard r-cran-shinycssloaders \
   r-cran-arrow r-cran-plotly \
   r-cran-sf r-cran-sfarrow r-cran-raster r-cran-leaflet \
-  r-cran-biocmanager r-cran-janitor r-cran-odbc r-cran-rgdal r-cran-rpostgres r-cran-srvyr r-cran-zoo
+  r-cran-biocmanager r-cran-janitor r-cran-odbc r-cran-rgdal r-cran-rpostgres r-cran-srvyr r-cran-zoo \
+  r-cran-bh r-cran-cli r-cran-cowplot r-cran-crosstalk r-cran-ggplot2 r-cran-lazyeval r-cran-openxlsx r-cran-reticulate r-cran-rlang r-cran-snakecase r-cran-viridis
+
+     
 
 # Python
 pip install \
