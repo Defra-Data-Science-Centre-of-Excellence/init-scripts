914a562cb88f244f704e82c3603378da4748c267
Bring salmon <128599540+pantelisindefra@users.noreply.github.com>
2024-02-27 13:09:58 +0000

    Create r_installs.R

---
+++ r_installs.R
@@ -0,0 +1,19 @@
+lib_list <- c("rlang", "renv", 
+              "devtools", "rstudioapi", 
+              "reticulate", "packrat", 
+              "rsconnect", "openxlsx", 
+              "arrow", "odbc", 
+              "RPostgres", "DT", 
+              "shinyjs", "shinydashboard", 
+              "shinycssloaders", "crosstalk",
+              "plotly", "ggplot2", 
+              "cowplot", "viridis",
+              "gdalBindings", "sf",
+              "sfarrow", "raster", 
+              "leaflet", "BiocManager", 
+              "janitor", "srvyr", 
+              "zoo", "bh", 
+              "cli", "lazyeval",
+              "snakecase"
+              )
+install.packages(lib_list, repos="https://cloud.r-project.org")
