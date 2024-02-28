b1c878a6e19d5d9c09dbb563962d98e80e35c2f1
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-02-28 15:59:05 +0000

    Update rootcert.sh

--- src/rootcert.sh
+++ src/rootcert.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-FILE="/dbfs/databricks/scripts/cscrootcerts.crt"
-
-# Split up and add certs to SSL
-awk 'BEGIN {c=0;} /-----BEGIN CERTIFICATE-----/ {c++} { print > "/usr/local/share/ca-certificates/cscrootcert-" c ".crt"}' < $FILE
+f0="/dbfs/databricks/scripts/cscrootcerts.crt"
+f1="/etc/ssl/certs/cscrootcert.crt"
+cp $f0 $f1
 update-ca-certificates
-
-# Add certs to Java
-KEYSTORE=$JAVA_HOME"lib/security/cacerts"
-for ALIAS in $(ls /etc/ssl/certs/cscrootcert-*.pem); do
-  echo "Info: not adding $ALIAS using keytool, requires storepass >6chars"
-  # keytool -noprompt -import -trustcacerts -alias $ALIAS -keystore $KEYSTORE -storepass $PASSWORD
-done
-
-# Append certs to worker nodes
-echo "export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt" >> /databricks/spark/conf/spark-env.sh
+echo "export REQUESTS_CA_BUNDLE=$f1" >> /databricks/spark/conf/spark-env.sh
 /databricks/spark/scripts/restart_dbfs_fuse_daemon.sh
