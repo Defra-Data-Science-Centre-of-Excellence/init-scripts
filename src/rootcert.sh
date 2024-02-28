f001b75b2d2541362cfd0c88792ff8e812cf6a75
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-02-28 18:18:03 +0000

    with quotes

--- src/rootcert.sh
+++ src/rootcert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 f0="/dbfs/databricks/scripts/cscrootcerts.crt"
-f1="/etc/ssl/certs/cscrootcert.crt"
-cp $f0 $f1
+f1="/etc/ssl/certs/ca-certificates.crt"
+cp "$f0" "$f1"
 update-ca-certificates
 echo "export REQUESTS_CA_BUNDLE=$f1" >> /databricks/spark/conf/spark-env.sh
 /databricks/spark/scripts/restart_dbfs_fuse_daemon.sh
