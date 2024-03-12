317c81ca74a26534a00d24ddf48b2f7bcf01bbbc
aw-west-defra <aw-west-defra>
2024-03-12 12:31:15 +0000

    don't exit on a fail and fixed typo

--- DASH.sh
+++ DASH.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -ex
+set -x
 echo $DB_CLUSTER_NAME
 URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src
 
-wget $URL/rootcerts.sh -O- | bash
+wget $URL/rootcert.sh -O- | bash
 wget $URL/update.sh -O- | bash 
 wget $URL/base_libs.sh -O- | bash
 wget $URL/gis_libs.sh -O- | bash
