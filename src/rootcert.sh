6e0150c99eee3e2fc0d72435fa7891d3e3e04d16
aw-west-defra <aw-west-defra>
2024-03-12 12:30:21 +0000

    keytool fails

--- src/rootcert.sh
+++ src/rootcert.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-FILE="/dbfs/databricks/scripts/cscrootcerts.crt"
-
-# Split up and add certs to SSL
-awk 'BEGIN {c=0;} /-----BEGIN CERTIFICATE-----/ {c++} { print > "/usr/local/share/ca-certificates/cscrootcert-" c ".crt"}' < $FILE
+# SSL
+#awk 'BEGIN {c=0;} /-----BEGIN CERTIFICATE-----/ {c++} { print > "/usr/local/share/ca-certificates/cscrootcert-" c ".pem"}' < "/dbfs/databricks/scripts/cscrootcerts.crt"
+cp "/dbfs/databricks/scripts/cscrootcerts.crt" "/usr/local/share/ca-certificates/cscrootcerts.crt"
 update-ca-certificates
 
-# Add certs to Java
-KEYSTORE=$JAVA_HOME"lib/security/cacerts"
-for ALIAS in $(ls /etc/ssl/certs/cscrootcert-*.pem); do
-  echo "Info: not adding $ALIAS using keytool, requires storepass >6chars"
-  # keytool -noprompt -import -trustcacerts -alias $ALIAS -keystore $KEYSTORE -storepass $PASSWORD
-done
+# Java
+#keytool -noprompt -import -trustcacerts -alias $ALIAS -keystore $KEYSTORE -storepass $PASSWORD
 
-# Append certs to worker nodes
+# Spark
 echo "export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt" >> /databricks/spark/conf/spark-env.sh
 /databricks/spark/scripts/restart_dbfs_fuse_daemon.sh
