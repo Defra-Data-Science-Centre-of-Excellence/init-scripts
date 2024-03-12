5a4df835d5d62ed561782373da1f2eae0b174ae8
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-03-12 18:02:31 +0000

    rest

--- src/rootcert.sh
+++ src/rootcert.sh
@@ -1,5 +1,5 @@
-# https://kb.databricks.com/python/import-custom-ca-cert
 cp "/dbfs/databricks/scripts/cscrootcerts.crt" "/usr/local/share/ca-certificates/myca.crt"
+# Rest is taken from https://kb.databricks.com/python/import-custom-ca-cert
 
 update-ca-certificates
 
