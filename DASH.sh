07536ef14d8f0818775355bda93b44e0d413ad12
aw-west-defra <aw-west-defra>
2024-03-12 14:21:48 +0000

    readded exit, and conditional base/gis lib installs

--- DASH.sh
+++ DASH.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-set -x
+set -ex
 echo $DB_CLUSTER_NAME
 URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src
 
 wget $URL/rootcert.sh -O- | bash
 wget $URL/update.sh -O- | bash 
-wget $URL/base_libs.sh -O- | bash
-wget $URL/gis_libs.sh -O- | bash
+if [[ "$DB_CLUSTER_NAME" == *"RStu"* ]]; then  # If RStu in cluster name
+  wget $URL/base_libs.sh -O- | bash
+fi
+if [[ "$DB_CLUSTER_NAME" == *"Geo"* ]]; then  # If Geo in cluster name
+  wget $URL/gis_libs.sh -O- | bash
+fi
