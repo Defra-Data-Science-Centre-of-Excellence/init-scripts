16800a43efdcb5c606b5936120b77a72199b3cd0
aw-west-defra <aw-west-defra>
2024-03-14 14:08:53 +0000

    recording issues

--- DASH.sh
+++ DASH.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-set -ex
+set -x
 echo $DB_CLUSTER_NAME
 
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/aw-fixes/src
 wget $URL/rootcert.sh -O- | bash -ex
-wget $URL/update.sh -O- | bash  -ex
-wget $URL/base_libs.sh -O- | bash  -ex
-if [[ "$DB_CLUSTER_NAME" == *"RStu"* ]]; then  # If RStu in cluster name
-  wget -qO- $URL/r_installs.R | Rscript -
-fi
-if [[ "$DB_CLUSTER_NAME" == *"Geo"* ]]; then  # If Geo in cluster name
-  wget $URL/gis_libs.sh -O- | bash
-fi
+wget $URL/update.sh -O- | bash -ex
+wget $URL/base_libs.sh -O- | bash -ex
+
+# works but very slow
+#wget -qO- $URL/r_installs.R | Rscript -
+
+# not installing with ssl
+wget $URL/gis_libs.sh -O- | bash -ex
+
+# with ssl this causes the detatch bug: "Failure starting repl."
+wget $URL/gdal.sh -qO- | bash -ex
+
+# conflicts for some libs, causing set -e exit
+pip install -Ur https://gist.githubusercontent.com/aw-west-defra/99ef9bb208ddb85e7f29381937d78cea/raw/1e340d4c4ed46031f08a6d5edb44c75602a80bb9/requirements.txt
