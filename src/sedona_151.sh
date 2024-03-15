adb9ca015a0d0fb43a423135dd501be710e4fcf9
Sam Flint <60146240+s-flint-defra@users.noreply.github.com>
2024-03-15 17:24:35 +0000

    Update sedona_151.sh

--- src/sedona_151.sh
+++ src/sedona_151.sh
@@ -4,12 +4,12 @@
 ### spark.serializer org.apache.spark.serializer.KryoSerializer
 ### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
 DIR=/databricks/jars
-SPARK="3.4"
+SPARK="3.5"
 SCALA="2.12"
 SEDONA="1.5.1"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.5.1-28.2/geotools-wrapper-1.5.1-28.2.jar"#"https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.4.0-28.2/geotools-wrapper-1.4.0-28.2.jar"
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
-wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
+wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-3.4_${SCALA}/1.4.1/sedona-viz-3.4_${SCALA}-1.4.1.jar"
 
 # Libraries required for python support 
 pip install \
