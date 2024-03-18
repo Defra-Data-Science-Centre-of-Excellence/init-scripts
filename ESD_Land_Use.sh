3f3c8c30f3f606afea3fecf224c45238a4ea5bef
Sam Flint <60146240+s-flint-defra@users.noreply.github.com>
2024-03-18 11:36:42 +0000

    Update ESD_Land_Use.sh

--- ESD_Land_Use.sh
+++ ESD_Land_Use.sh
@@ -8,4 +8,4 @@ wget $URL/update.sh -O- | bash
 wget $URL/c_dependencies.sh -O- | bash 
 wget $URL/sedona_150.sh -O- | bash 
 
-pip install /Workspace/Shared/python_libraries/sds_dash_download-0.1.0-py3-none-any.whl
+pip install /Workspace/Shared/python_libraries/sds_dash_download-0.1.0-py3-none-any.whl /Workspace/Shared/python_libraries/sds_land_use_model-0.1.0-py3-none-any.whl
