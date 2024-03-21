8ae60dacc585005922b2f8e1535934615e7d3696
Sam Flint <60146240+s-flint-defra@users.noreply.github.com>
2024-03-21 09:49:12 +0000

    pin package versions

--- ESD_Land_Use latest sedona.sh
+++ ESD_Land_Use latest sedona.sh
@@ -11,12 +11,5 @@ wget $URL/sedona_151.sh -O- | bash
 pip install /Workspace/Shared/python_libraries/sds_dash_download-0.1.0-py3-none-any.whl
 
 # other libraries
-pip install pyogrio==0.7.2
-pip install keplergl==0.3.2
-pip install pydeck==0.8.0
-pip install geopandas==0.14.2
-pip install pandana==0.7
-pip install shapely==2.0.2
-pip install matplotlib==3.8.3
-pip install mapclassify==2.6.1
-pip install folium==0.15.1
+pip install pyogrio==0.7.2 keplergl==0.3.2 pydeck==0.8.0 geopandas==0.14.2 pandana==0.7 shapely==2.0.2 matplotlib==3.8.3 mapclassify==2.6.1 folium==0.15.1 rasterio==1.3.9 xarray==2024.02.0 rioxarray==
+0.15.1 zarr==2.17.1
