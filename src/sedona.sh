8cefc07e8603b8058b7c4e40f6556728b4b84bd0
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-04-17 14:07:49 +0100

    removed sedona and rename gis_libs.sh to sedona.sh

--- src/sedona.sh
+++ src/sedona.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # Sedona
-## requires spark config:
-### spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
-### spark.serializer org.apache.spark.serializer.KryoSerializer
-### spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
 DIR=/databricks/jars
 SPARK="3.4"
 SCALA="2.12"
@@ -12,6 +8,3 @@ wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/datasy
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
 pip install apache-sedona==$SEDONA
-
-# Mosaic
-pip install databricks-mosaic
