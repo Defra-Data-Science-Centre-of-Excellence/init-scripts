d410be6a1e1c4c4b9b967fde2bae44d4160d3049
Sam Flint <60146240+s-flint-defra@users.noreply.github.com>
2024-05-24 17:52:07 +0100

    Update sedona_160.sh

--- src/sedona_160.sh
+++ src/sedona_160.sh
@@ -7,8 +7,8 @@ DIR=/databricks/jars
 SPARK="3.5"
 SCALA="2.12"
 SEDONA="1.6.0"
-wget $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.6.0-28.2/geotools-wrapper-1.6.0-28.2.jar"
-wget $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
+wget --no-check-certificate -NP  $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.6.0-28.2/geotools-wrapper-1.6.0-28.2.jar"
+wget --no-check-certificate -NP  $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
 
 # Libraries required for python support 
 pip install \
