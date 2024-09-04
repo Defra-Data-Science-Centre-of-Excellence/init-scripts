644ce95a07a879e74feabd36e2a780f4a0a8fb1e
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-09-04 12:21:25 +0100

    use conda-forge over default

--- src/gdal.sh
+++ src/gdal.sh
@@ -2,10 +2,14 @@
 DIR=/databricks/miniconda
 mkdir -p $DIR
 
+# Install Conda
 wget --no-check-certificate -qNP $DIR https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash $DIR/Miniconda3-latest-Linux-x86_64.sh -bufp $DIR
 
+# Use Conda Forge
 $DIR/bin/conda config --set ssl_verify false
+$DIR/bin/conda config --add channels conda-forge
+$DIR/bin/conda config --set channel_priority strict
 $DIR/bin/conda update -y --all
 $DIR/bin/conda install -y -c conda-forge libgdal-arrow-parquet
 
