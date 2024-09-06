0e1a7e1e3889edb71eba552536c9a47788ee06d2
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-09-06 11:18:43 +0100

    checked geotools

--- src/sedona.sh
+++ src/sedona.sh
@@ -4,11 +4,11 @@
 #spark.serializer org.apache.spark.serializer.KryoSerializer
 #spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
 DIR=/databricks/jars
-SPARK="3.4"
+GEOTOOLS="28.2"
+SPARK="3.5"
 SCALA="2.12"
 SEDONA="1.6.1"
-GEOTOOLS="1.6.1-28.2"
-wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/${GEOTOOLS}/geotools-wrapper-${GEOTOOLS}.jar"
+wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/$SEDONA-$GEOTOOLS/geotools-wrapper-$SEDONA-$GEOTOOLS.jar"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
 pip install apache-sedona==$SEDONA keplergl==0.3.2 pydeck==0.8.0 geopandas==0.11.1
