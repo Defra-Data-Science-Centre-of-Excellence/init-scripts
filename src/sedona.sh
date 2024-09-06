f28de6177d5b16fa4be517e7a5fef7e916e80d4f
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-09-06 11:00:10 +0100

    geotools arg

--- src/sedona.sh
+++ src/sedona.sh
@@ -3,8 +3,9 @@
 DIR=/databricks/jars
 SPARK="3.4"
 SCALA="2.12"
-SEDONA="1.4.1"
-wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.4.0-28.2/geotools-wrapper-1.4.0-28.2.jar"
+SEDONA="1.6.1"
+GEOTOOLS="1.4.0-28.2"
+wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/${GEOTOOLS}/geotools-wrapper-${GEOTOOLS}.jar"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
-pip install apache-sedona==$SEDONA
+pip install apache-sedona==$SEDONA keplergl==0.3.2 pydeck==0.8.0
