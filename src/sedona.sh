d7e89bfee32275f443c37512cbe1aa87978bd1da
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-09-06 11:06:23 +0100

    Sedona v1.6

--- src/sedona.sh
+++ src/sedona.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Sedona
+#spark.sql.extensions org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
+#spark.serializer org.apache.spark.serializer.KryoSerializer
+#spark.kryo.registrator org.apache.sedona.core.serde.SedonaKryoRegistrator
 DIR=/databricks/jars
 SPARK="3.4"
 SCALA="2.12"
 SEDONA="1.6.1"
-GEOTOOLS="1.4.0-28.2"
+GEOTOOLS="1.6.1-28.2"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/${GEOTOOLS}/geotools-wrapper-${GEOTOOLS}.jar"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-${SPARK}_${SCALA}/$SEDONA/sedona-spark-shaded-${SPARK}_${SCALA}-$SEDONA.jar"
 wget --no-check-certificate -qNP $DIR "https://repo1.maven.org/maven2/org/apache/sedona/sedona-viz-${SPARK}_${SCALA}/$SEDONA/sedona-viz-${SPARK}_${SCALA}-$SEDONA.jar"
-pip install apache-sedona==$SEDONA keplergl==0.3.2 pydeck==0.8.0
+pip install apache-sedona==$SEDONA keplergl==0.3.2 pydeck==0.8.0 geopandas==0.11.1
