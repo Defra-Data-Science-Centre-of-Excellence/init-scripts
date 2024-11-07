91cab558849da328d82a113342778ad82dd45ad7
tomkdefra <tomkdefra>
2024-11-07 10:15:45 +0000

    Added some documentation comments

--- test/esa_snap_plus_snappy_configuration.sh
+++ test/esa_snap_plus_snappy_configuration.sh
@@ -2,7 +2,8 @@
 
 INSTALLER_DBFS_PATH="/dbfs/mnt/lab/unrestricted/admin/apps/esa_snap_sentinel_linux_10_0_0.sh"
 INSTALLER_TMP_PATH="/tmp/esa_snap_sentinel_linux_10_0_0.sh"
-SNAP_INSTALL_DIR="/opt/esa-snap"
+SNAPPY_CONFIG_FILE="/opt/esa-snap/bin/snappy-conf"
+SNAPPY_MODULE="/root/.snap/snap-python/esa_snappy"
 
 # Wait until mount is accessible
 # Trying to get the install script from the lab zone fails without this wait process, possibly because
@@ -18,9 +19,6 @@ while [ ! -f "$INSTALLER_DBFS_PATH" ]; do
   ELAPSED=$((ELAPSED + WAIT_INTERVAL))
 done
 
-# Copying the installer file from the mounted Lab zone to local storage on the driver node. 
-# I don't know if this is completely necessary, but it's a big file and running the install 
-# straight from Lab might be less reliable. 
 cp "$INSTALLER_DBFS_PATH" "$INSTALLER_TMP_PATH"
 if [ $? -ne 0 ]; then
   echo "Error: Failed to copy installer script to $INSTALLER_TMP_PATH."
@@ -28,8 +26,6 @@ if [ $? -ne 0 ]; then
 fi
 
 # Detect the Python executable dynamically
-# The Python exec seems to be all over the place.. watch the log statements from this install
-# and then run `which python3` in a notebook and see...
 PYTHON_EXEC=$(which python3)
 if [ -z "$PYTHON_EXEC" ]; then
   echo "Error: Failed to detect the Python executable."
@@ -37,9 +33,7 @@ if [ -z "$PYTHON_EXEC" ]; then
 fi
 echo "Excellent! We found the Python executable at $PYTHON_EXEC"
 
-# Construct the site-packages path. Because ^above^ this is also all over the place,
-# which is why I haven't been able to figure out the esa-snappy configuration step in 
-# this init script.
+# Construct the site-packages path
 PYTHON_SITE_PACKAGES="${PYTHON_EXEC%/bin/python3}/lib/python${PYTHON_VERSION}/site-packages"
 echo "The Python site-packages directory is located at: $PYTHON_SITE_PACKAGES"
 
@@ -54,3 +48,42 @@ y
 $PYTHON_EXEC
 y
 EOF
+echo "The SNAP installation ought to have completed. Now we wait for $SNAPPY_CONFIG_FILE"
+
+TIMEOUT=120
+WAIT_INTERVAL=5
+ELAPSED=0
+while [ ! -f "$SNAPPY_CONFIG_FILE" ]; do
+  if [ $ELAPSED -ge $TIMEOUT ]; then
+    exit 1
+  fi
+  sleep $WAIT_INTERVAL
+  ELAPSED=$((ELAPSED + WAIT_INTERVAL))
+done
+echo "The wait loop has completed and found $SNAPPY_CONFIG_FILE"
+
+# Configure SNAP for Python
+echo "Attempting to configure SNAP with Python using $SNAPPY_CONFIG_FILE and $PYTHON_EXEC"
+echo "Starting snappy-conf..."
+"$SNAPPY_CONFIG_FILE" "$PYTHON_EXEC"
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "Error: snappy-conf failed with exit code $EXIT_CODE."
+  exit $EXIT_CODE
+fi
+echo "Finished snappy-conf."
+
+# Try the wait loop on the esa snappy module before copying it to site-packages
+TIMEOUT=120
+WAIT_INTERVAL=5
+ELAPSED=0
+while [ ! -f "$SNAPPY_MODULE" ]; do
+  if [ $ELAPSED -ge $TIMEOUT ]; then
+    exit 1
+  fi
+  sleep $WAIT_INTERVAL
+  ELAPSED=$((ELAPSED + WAIT_INTERVAL))
+done
+
+# Copy the SNAP Python module
+cp -r "$SNAPPY_MODULE" "$PYTHON_PACKAGES"
