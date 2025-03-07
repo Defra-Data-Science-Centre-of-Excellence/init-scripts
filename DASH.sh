3d6dca3e2e8a2a25ca0d91d5a17873793ec1455f
abeleno-defra <abeleno-defra>
2025-03-07 16:06:07 +0000

    Added Rcpp

--- DASH.sh
+++ DASH.sh
@@ -5,7 +5,7 @@ echo $DB_CLUSTER_NAME
 # Optional cscrootcerts.crt path
 export CERT_PATH="/Volumes/prd_dash_config/common/init_scripts/src/cscrootcert.crt"
 
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/uc-workspaces/src
 wget $URL/rootcert.sh -O- | bash -ex
 wget $URL/update.sh -O- | bash  -ex
 wget $URL/base_libs.sh -O- | bash  -ex
